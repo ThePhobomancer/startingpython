c3ec1582047267a0036f7067105f7d48dc8b282d
ThePhobomancer <valmir.koji@gmail.com>
2025-04-21 00:24:35 +0900

    Practiced adding to the list of names in multiple ways

--- rng.py
+++ rng.py
@@ -3,3 +3,11 @@ import random
 rng_list = ["alice", "bart", "clyde", "drake"]
 randomelement = random.choice(rng_list)
 print(randomelement)
+
+rng_list.append('Eric')
+rng_list.extend(['Frank', 'Gloria', 'Helen'])
+rng_list.insert(1, 'Zeke')
+
+randomelement2 = random.choice(rng_list)
+print(randomelement2)
+print(rng_list)
