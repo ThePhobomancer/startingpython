2f0d0ee07f01ac27eeeb178477f46ce4d05f6afa
ThePhobomancer <valmir.koji@gmail.com>
2025-04-22 19:43:09 +0900

    human generator

--- human_characteristics_generator.py
+++ human_characteristics_generator.py
@@ -6,4 +6,4 @@ for i in range(0,10):
     human.append(i)
     human[i] = 20 + random.randint(1,10) + random.randint(1,10)
 print(human)
-print("MS",human[0],"|BS",human[1],"|S",human[2],"|T",human[3],"|I",human[4],"|Ag",human[5],"|Dex",human[6],"|Int",human[7],"|WP",human[8],"|Fel",human[9])
+print("WS",human[0],"|BS",human[1],"|S",human[2],"|T",human[3],"|I",human[4],"|Ag",human[5],"|Dex",human[6],"|Int",human[7],"|WP",human[8],"|Fel",human[9])
