60d6573b1fbc1f02588552f1ace5b397aa27482f
ThePhobomancer <valmir.koji@gmail.com>
2025-04-27 22:58:23 +0900

    Practiced imports with cowsay

--- hello.py
+++ hello.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 
 import requests
+import cowsay
 
 print("hello world")
 
 print(requests.get("https://google.com/").text)
+
+##from cowsay import cowsay
+##message = """moo""".strip()
+##print(cowsay(message))
+cowsay.cow("hello")
